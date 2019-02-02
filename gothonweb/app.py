d3f76390f813233ca1eda767e3c2b6d10a51d608
tank7575 <gtanke@gmail.com>
2019-02-02 15:55:04 -0600

    none

--- gothonweb/app.py
+++ gothonweb/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello', methods=['POST', 'GET'])
 def index():
-    greeting = "Hello World"
-    return render_template("index.html", greeting=greeting)
+    greeting = 'Hello World'
+
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = "{} {}".format(name, greet)
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("hello_form.html")
 
 if __name__ == "__main__":
     app.run()
+

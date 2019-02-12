4c8eb0aa8d9c2a8d91b2cf0ec013a2a51aba0644
tank7575 <gtanke@gmail.com>
2019-02-12 12:56:30 -0600

    none

--- Kivy/main.py
+++ Kivy/main.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
---
+++ overflow1.py
@@ -0,0 +1,16 @@
+i = 1
+
+while True:
+    print(i)
+    i = i * 2
+    if i == 524288:
+        break
+
+
+
+
+
+
+#for i == 1
+
+#if i == 1 and i * 2:

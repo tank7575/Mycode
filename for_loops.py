b5669db103151c1c0038daddd745f3910114c5e8
tank7575 <tank7575@yahoo.com>
2019-02-24 14:18:09 -0600

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,33 @@
+fruits = ["apples", "banana", "cherry"]
+#for x in fruits:
+ #   print(x)
+
+#for x in "banana":
+    #print(x)
+
+#for x in fruits:
+  #  print(x)
+   # if x == "banana":
+    #    break
+
+#for x in fruits:
+ #   if x == "banana":
+  #      break
+   # print(x)
+
+#for x in range(6):
+ #   print(x)
+
+#for x in range(2, 6):
+ #   print(x)
+
+#for x in range(2, 30, 3):
+    #print(x)
+#for x in range(6):
+ #   print(x)
+#else:
+    #print("Finally finished!")
+adj = ['red', 'big', 'tasty']
+for x in adj:
+    for y in fruits:
+        print(x,y)
